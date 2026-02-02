fb73137a0ca06d536ee171a2135e9c1c513dbf7a
IvanP <p2.2011@yandex.ru>
2026-02-02 22:02:56 +0300

    init: project with front controller and PSR-4

---
+++ README.md
@@ -0,0 +1,4 @@
+# PHP MVC Blog
+
+блог на PHP 8.1 с MVC, ООП, Nginx, Docker и CI/CD.
+
---
+++ app/Core/App.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+final class App
+{
+    public function run(): void
+    {
+        echo 'Blog MVC ';
+    }
+}
